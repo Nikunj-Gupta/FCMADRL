ddd4e5c191912a43a4f0e6269855b85b198ddd56
Nikunj-Gupta <Nikunj-Gupta@github.com>
2019-08-01 16:09:25 +0530

    parameters specified

---
+++ config.py
@@ -0,0 +1,43 @@
+ENV_NAME = "FCMARL" 
+
+GAMMA = 0.95
+LEARNING_RATE = 0.001
+
+MEMORY_SIZE = 1000000
+BATCH_SIZE = 20
+
+EXPLORATION_MAX = 1.0
+EXPLORATION_MIN = 0.01
+EXPLORATION_DECAY = 0.995
+
+
+def make_env(scenario_name, benchmark=False):
+    from multiagent.environment import MultiAgentEnv
+    import multiagent.scenarios as scenarios
+
+    # load scenario from script
+    scenario = scenarios.load(scenario_name + ".py").Scenario()
+    # create world
+    world = scenario.make_world()
+    # create multiagent environment
+    if benchmark:        
+        env = MultiAgentEnv(world, scenario.reset_world, scenario.reward, scenario.observation, scenario.benchmark_data)
+    else:
+        env = MultiAgentEnv(world, scenario.reset_world, scenario.reward, scenario.observation)
+    # print ("Hello Simple_Spread Env") 
+ 
+    return env 
+
+env = make_env("simple_spread") 
+
+
+#specify parameters here:
+episodes=30 
+steps=5 
+CA_OBS_SPACE = env.n 
+CA_ACTION_SPACE = 5 # env.observation_space[0].shape[0] 
+SA_OBS_SPACE = CA_ACTION_SPACE + env.observation_space[0].shape[0] + 1 
+SA_ACTION_SPACE = env.action_space[0].n 
+CA_ACTION_BOUND = 5.0 
+is_batch_norm = False #batch normalization switch
+DONE_VALUE = 0.0 
