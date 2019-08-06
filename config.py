83bd8f523c9e7f3b4a6c6d7b1d0c7e0a3f9d581e
Nikunj <guptanikunj1997@gmail.com>
2019-08-06 11:35:17 +0530

    updated

--- config.py
+++ config.py
@@ -8,8 +8,12 @@ BATCH_SIZE = 20
 
 EXPLORATION_MAX = 1.0
 EXPLORATION_MIN = 0.01
-EXPLORATION_DECAY = 0.995
+EXPLORATION_DECAY = 0.99 
 
+FILE_PATH_DQN = "dqn/model/"
+FILE_NAME_DQN = "model0.h5"
+CHECKPOINT = 1
+LOG_PATH = "logs/"
 
 def make_env(scenario_name, benchmark=False):
     from multiagent.environment import MultiAgentEnv
@@ -25,15 +29,14 @@ def make_env(scenario_name, benchmark=False):
     else:
         env = MultiAgentEnv(world, scenario.reset_world, scenario.reward, scenario.observation)
     # print ("Hello Simple_Spread Env") 
- 
     return env 
 
 env = make_env("simple_spread") 
 
 
 #specify parameters here:
-episodes=30000 
-steps=200 
+episodes=5 
+steps=5 
 CA_OBS_SPACE = env.n 
 CA_ACTION_SPACE = 5 # env.observation_space[0].shape[0] 
 SA_OBS_SPACE = CA_ACTION_SPACE + env.observation_space[0].shape[0] + 1 
